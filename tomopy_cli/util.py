ba7d2117407ffe9ebea3a8ee636e753439d6c96e
Francesco De Carlo <decarlof@gmai.com>
2020-01-04 12:58:54 -0600

    added rotation_axis.json to config. Docs clean-up

--- tomopy_cli/util.py
+++ tomopy_cli/util.py
@@ -1,4 +1,7 @@
+import tomopy
+
 from tomopy_cli import log
+from tomopy_cli import file_io
 
 def theta_step(start, end, proj_number):
     return (end-start)/proj_number
@@ -41,3 +44,28 @@ def restricted_float(x):
     if x < 0.0 or x >= 1.0:
         raise argparse.ArgumentTypeError("%r not in range [0.0, 1.0]"%(x,))
     return x
+
+def find_rotation_axis(fname, nsino):
+    
+    log_lib.info("  *** calculating automatic center")
+    data_size = file_io.get_dx_dims(fname, 'data')
+    ssino = int(data_size[1] * nsino)
+
+    # Select sinogram range to reconstruct
+    start = ssino
+    end = start + 1
+    sino = (start, end)
+
+    # Read APS 32-BM raw data
+    proj, flat, dark, theta = dxchange.read_aps_32id(fname, sino=sino)
+        
+    # Flat-field correction of raw data
+    data = tomopy.normalize(proj, flat, dark, cutoff=1.4)
+
+    # remove stripes
+    data = tomopy.remove_stripe_fw(data,level=5,wname='sym16',sigma=1,pad=True)
+
+    # find rotation center
+    rot_center = tomopy.find_center_vo(data)   
+    log_lib.info("  *** automatic center: %f" % rot_center)
+    return rot_center
