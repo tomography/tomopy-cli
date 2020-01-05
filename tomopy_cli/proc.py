19ea424ca9dc403d06643cd0cd57525a706c0b27
Francesco De Carlo <decarlof@gmai.com>
2020-01-05 16:54:11 -0600

    first commit

---
+++ tomopy_cli/proc.py
@@ -0,0 +1,40 @@
+import tomopy
+import numpy as np
+
+from tomopy_cli import log
+
+def mask(data, params):
+    
+    if(params.reconstruction_mask):
+        if 0 < params.reconstruction_mask_ratio <= 1:
+            log.warning("  *** apply reconstruction mask ratio: %f " % params.reconstruction_mask_ratio)
+            data = tomopy.circ_mask(data, axis=0, ratio=params.reconstruction_mask_ratio)
+        else:
+            log.error("  *** mask ratio must be between 0-1: %f is ignored" % params.reconstruction_mask_ratio)
+
+    return data
+
+def reconstruct(data, theta, rot_center, params):
+
+    log.info("  *** algorithm: %s" % params.reconstruction_algorithm)
+    if params.reconstruction_algorithm == 'astrasirt':
+        extra_options ={'MinConstraint':0}
+        options = {'proj_type':'cuda', 'method':'SIRT_CUDA', 'num_iter':200, 'extra_options':extra_options}
+        shift = (int((data.shape[2]/2 - rot_center)+.5))
+        data = np.roll(data, shift, axis=2)
+        rec = tomopy.recon(data, theta, algorithm=tomopy.astra, options=options)
+    elif params.reconstruction_algorithm == 'astracgls':
+        extra_options ={'MinConstraint':0}
+        options = {'proj_type':'cuda', 'method':'CGLS_CUDA', 'num_iter':15, 'extra_options':extra_options}
+        shift = (int((data.shape[2]/2 - rot_center)+.5))
+        data = np.roll(data, shift, axis=2)
+        rec = tomopy.recon(data, theta, algorithm=tomopy.astra, options=options)
+    elif params.reconstruction_algorithm == 'gridrec':
+        rec = tomopy.recon(data, theta, center=rot_center, algorithm=params.reconstruction_algorithm, filter_name=params.filter)
+    else:
+        log.warning("  *** *** algorithm: %s is not supported yet" % params.reconstruction_algorithm)
+        params.reconstruction_algorithm = 'gridrec'
+        log.warning("  *** *** using: %s instead" % params.reconstruction_algorithm)
+        rec = tomopy.recon(data, theta, center=rot_center, algorithm=params.reconstruction_algorithm, filter_name=params.filter)
+
+    return rec
