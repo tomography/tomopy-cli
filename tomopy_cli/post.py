26110405f8cb1f9367cccdd1b77252ad220f4c0a
Francesco De Carlo <decarlof@gmai.com>
2020-01-12 14:58:07 -0600

    setup post.py to contain all post reconstuction processing tasks

--- tomopy_cli/post.py
+++ tomopy_cli/post.py
@@ -14,12 +14,17 @@ def segment(params):
     top = os.path.dirname(params.hdf_file) + '_rec' + tail
 
     # log.info(os.listdir(top))
-    rec_file_list = list(filter(lambda x: x.endswith(('.tiff', '.tif')), os.listdir(top)))
-    rec_file_list.sort()
+    if os.path.isdir(top):
+        rec_file_list = list(filter(lambda x: x.endswith(('.tiff', '.tif')), os.listdir(top)))
+        rec_file_list.sort()
 
 
-    log.info('found: %s' % rec_file_list)
-    log.info('applying segmentation')
-
-    log.error('not implemented')
+        log.info('found in %s' % top)
+        log.info('files %s' % rec_file_list)
+        log.info('applying segmentation')
+        log.warning('not implemented')
+    else:
+        log.error("ERROR: the directory %s does not exist" % top)
+        log.error("ERROR: to create one run a full reconstruction first:")
+        log.error("ERROR: $ tomopy recon --reconstruction-type full --hdf-file %s" % params.hdf_file)
 
