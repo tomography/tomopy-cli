38f0d45a0bc5bef8c224483a1274538704e422dc
Francesco De Carlo <decarlof@gmai.com>
2020-01-12 14:24:13 -0600

    setup post.py to contain all post reconstuction processing tasks

--- tomopy_cli/post.py
+++ tomopy_cli/post.py
@@ -1,6 +1,25 @@
+import os
 import tomopy
 import numpy as np
 
 from tomopy_cli import log
 
+
+# this module will host post reconstuction data analysis (segmentation, etc.)
+
+def segment(params):
+
+    # slice/full reconstruction file location
+    tail = os.sep + os.path.splitext(os.path.basename(params.hdf_file))[0]+ '_full_rec' + os.sep 
+    top = os.path.dirname(params.hdf_file) + '_rec' + tail
+
+    # log.info(os.listdir(top))
+    rec_file_list = list(filter(lambda x: x.endswith(('.tiff', '.tif')), os.listdir(top)))
+    rec_file_list.sort()
+
+
+    log.info('found: %s' % rec_file_list)
+    log.info('applying segmentation')
+
+    log.error('not implemented')
 
