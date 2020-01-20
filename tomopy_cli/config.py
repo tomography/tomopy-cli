34792a6fc88779df186d43281307a63971ba650f
Francesco De Carlo <decarlof@gmail.com>
2020-01-20 13:47:45 -0600

    lock minus-log and fw-pad to True

--- tomopy_cli/config.py
+++ tomopy_cli/config.py
@@ -165,7 +165,7 @@ SECTIONS['flat-correction'] = {
         'type': float,
         'help': "Values to be replaced with negative values in array"},
     'minus-log': {
-        'default': False,
+        'default': True,
         'help': "Minus log",
         'action': 'store_true'},
         }
@@ -221,7 +221,7 @@ SECTIONS['fw'] = {
         'default': 7,
         'help': "Fourier-Wavelet remove stripe level parameter"},
     'fw-pad': {
-        'default': False,
+        'default': True,
         'help': "When set, Fourier-Wavelet remove stripe extend the size of the sinogram by padding with zeros",
         'action': 'store_true'},
     }
