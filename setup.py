214b399af5a08462a49787f1d1805f9dae2eb76a
Francesco De Carlo <decarlof@gmai.com>
2020-02-12 10:22:15 -0600

    removed sacred test

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     url='https://github.com/decarlof/tomopy-cli',
     packages=find_packages(),
     include_package_data = True,
-    scripts=['bin/tomopy', 'bin/tomopy-sacred.py'],
+    scripts=['bin/tomopy'],
     description='cli for tomopy',
     zip_safe=False,
     cmdclass={'install': PostInstallCommand},
