f18f80544baa3efd621f620075c130cd94a559a4
Mark Wolfman <canismarko@gmail.com>
2020-05-03 12:10:09 -0500

    Path to tiff output folder now uses absolute paths.
    This fixes an edge case where reconstructing an HDF5 in the current
    directory (e.g. './test_tomogram.hdf5') will save outputs in the
    unhelpfully named ``._rec`` directory instead of whatever the
    directory's actual name is.

--- tests/test_recon.py
+++ tests/test_recon.py
@@ -1,5 +1,7 @@
 from unittest import TestCase, mock
 import os
+import shutil
+from pathlib import Path
 
 import numpy as np
 import h5py
@@ -7,13 +9,12 @@ import h5py
 import tomopy
 from tomopy_cli.recon import rec
 
-
-HDF_FILE = './test_tomogram.h5'
+HDF_FILE = Path(__file__).resolve().parent / 'test_tomogram.h5'
 
 
 def make_params():
     params = mock.MagicMock()
-    params.file_name = HDF_FILE
+    params.file_name = str(HDF_FILE)
     params.rotation_axis = 32
     params.file_type = 'standard'
     params.file_format = 'dx'
@@ -26,22 +27,30 @@ def make_params():
 
 
 class ReconTests(TestCase):
+    output_dir = Path(__file__).resolve().parent.parent / 'tests_rec'
+
     def setUp(self):
         # Prepare some dummy data
         phantom = tomopy.misc.phantom.shepp3d(size=64)
         phantom = np.exp(-phantom)
         flat = np.ones((2, *phantom.shape[1:]))
         dark = np.zeros((2, *phantom.shape[1:]))
-        with h5py.File(HDF_FILE, mode='w-') as fp:
+        with h5py.File(str(HDF_FILE), mode='w-') as fp:
             fp.create_dataset('/exchange/data', data=phantom)
             fp.create_dataset('/exchange/data_white', data=flat)
             fp.create_dataset('/exchange/data_dark', data=dark)
     
     def tearDown(self):
-        if os.path.exists(HDF_FILE):
-            os.remove(HDF_FILE)
+        # Remove the temporary HDF5 file
+        if os.path.exists(str(HDF_FILE)):
+            os.remove(str(HDF_FILE))
+        # Remove the reconstructed output
+        if self.output_dir.exists():
+            shutil.rmtree(self.output_dir)
         
     def test_basic_reconstruction(self):
+        """Check that a basic reconstruction completes and produces output tiff files."""
         params = make_params()
         params.reconstruction_type = 'slice'
         response = rec(params=params)
+        self.assertTrue(self.output_dir.exists())
