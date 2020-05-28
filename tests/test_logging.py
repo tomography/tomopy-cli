fa7d2b4e89d4d4a60bc267981f56c8ad698ece13
Mark Wolfman <canismarko@gmail.com>
2020-05-28 13:48:37 -0500

    Switched ``setup_custom_logger`` to better support the built-in logging module.
    Previously, all logging was done from the log.py file. This worked but
    had the down-side of losing many of the features of the standard
    library logging module, like recording line numbers. This change
    restores that functionality, while preserving the color output for all
    "tomopy_cli.*" loggers once ``setup_custom_logger`` has been called.
    
    Also added a deprecation warning if any of the ``info()`` functions in
    log.py are called directly.

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -12,15 +12,14 @@ from tomopy_cli import log as tplogging
 class LogFormattingTests(unittest.TestCase):
     temp_logfile = 'tests-temp.log'
     
-    def setUp(self):
-        self.log_capture = io.StringIO()
-        ch = logging.StreamHandler(self.log_capture)
-        tplogging.logger.addHandler(ch)
+    # def setUp(self):
+    #     self.log_capture = io.StringIO()
+
     
-    def tearDown(self):
-        # Remove temporary log files
-        if os.path.exists(self.temp_logfile):
-            os.remove(self.temp_logfile)
+    # def tearDown(self):
+    #     # Remove temporary log files
+    #     if os.path.exists(self.temp_logfile):
+    #         os.remove(self.temp_logfile)
     
     def test_setup_custom_logger(self):
         """Verify that setting up a custom logger produces formatted output."""
@@ -28,11 +27,11 @@ class LogFormattingTests(unittest.TestCase):
         stderr_io = io.StringIO()
         old_stderr = sys.stderr
         sys.stderr = stderr_io
-        # Do some logging both with and without setting up custom logger
         try:
-            tplogging.warning("No danger, Will Robinson")
+            # Do some logging after setting up custom logger
+            logger = logging.getLogger('tomopy_cli.fancy_logger')
             tplogging.setup_custom_logger(lfname=None)
-            tplogging.warning("Formatted danger, Will Robinson")
+            logger.warning("Formatted danger, Will Robinson")
         finally:
             # Restore stderr
             sys.stderr = old_stderr
@@ -40,13 +39,13 @@ class LogFormattingTests(unittest.TestCase):
         stderr_io.seek(0)
         loglines = stderr_io.readlines()
         self.assertEqual(len(loglines), 1)
-        self.assertNotIn("No danger", loglines[0])
         self.assertIn("\x1b[33m", loglines[0])
         self.assertIn("\x1b[0m", loglines[0])
     
     def test_colored_log_formatter(self):
+        log_capture = io.StringIO()
         # Logging without colored formatting
-        ch = logging.StreamHandler(self.log_capture)
+        ch = logging.StreamHandler(log_capture)
         logger = logging.getLogger('_test_logger')
         logger.addHandler(ch)
         logger.warning("Danger, Will Robinson")
@@ -54,8 +53,8 @@ class LogFormattingTests(unittest.TestCase):
         ch.setFormatter(tplogging.ColoredLogFormatter('%(asctime)s - %(message)s'))
         logger.warning("Formatted danger, Will Robinson")
         # Check the logged output for formatting
-        self.log_capture.seek(0)
-        loglines = self.log_capture.readlines()
+        log_capture.seek(0)
+        loglines = log_capture.readlines()
         self.assertNotIn("\x1b[33m", loglines[0])
         self.assertNotIn("\x1b[0m", loglines[0])
         self.assertIn("\x1b[33m", loglines[1])
--- tomopy_cli/log.py
+++ tomopy_cli/log.py
@@ -1,27 +1,57 @@
-'''
-Customized logging for the tomopy_cli library.
+'''Customized logging for the tomopy_cli library.
+
+Logging in tomopy_cli is built upon the standard logging functionality
+in python. This module provides a ``getLogger`` module that can be
+used to get a logger object with the usual *debug*, *info*, etc.,
+methods. If ``setup_custom_logger`` is called, all subsequent calls to
+``getLogger`` will produce a logger that reflects these
+customizations.
 
 '''
 import logging
+import warnings
 
 
 logger = logging.getLogger(__name__)
 
 
+def old_logging(func):
+    def inside(*args, **kwargs):
+        warnings.warn(
+            "``tomopy_cli.log.{}`` is deprecated, use the python logging module directly."
+            "".format(str(func)[10:17]),
+            DeprecationWarning,
+        )
+        return func(*args, **kwargs)
+    return inside
+
+
+@old_logging
+def debug(msg):
+    logger.debug(msg)
+
+
+@old_logging
 def info(msg):
     logger.info(msg)
 
 
+@old_logging
 def error(msg):
     logger.error(msg)
 
 
+@old_logging
 def warning(msg):
     logger.warning(msg)
 
 
-def setup_custom_logger(lfname: str=None, stream_to_console: bool=True):
+def setup_custom_logger(lfname: str=None, stream_to_console: bool=True, level=logging.DEBUG):
     """Prepare the logging system with custom formatting.
+    
+    This adds handlers to the *tomopy_cli* parent logger. Any logger
+    inside tomopy_cli will produce output based on this functions
+    customization parameters.
     
     Parameters
     ----------
@@ -31,33 +61,50 @@ def setup_custom_logger(lfname: str=None, stream_to_console: bool=True):
     stream_to_console
       If true, logs will be output to the console with color
       formatting.
+    level
+      A logging level for the handler. This can be either a string
+      ("DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"), or an actual
+      level defined in the python logging framework.
     
     """
+    parent_name = __name__.split('.')[0]  # Nominally "tomopy_cli"
+    parent_logger = logging.getLogger(parent_name)
+    parent_logger.setLevel(level)
+    # Set up normal output to a file
     if lfname is not None:
         fHandler = logging.FileHandler(lfname)
-        logger.setLevel(logging.DEBUG)
-        file_formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
+        file_formatter = logging.Formatter('%(asctime)s - %(name)s(%(lineno)s) - %(levelname)s: %(message)s')
         fHandler.setFormatter(file_formatter)
-        logger.addHandler(fHandler)
+        parent_logger.addHandler(fHandler)
+    # Set up formatted output to the console
     if stream_to_console:
         ch = logging.StreamHandler()
         ch.setFormatter(ColoredLogFormatter('%(asctime)s - %(message)s'))
-        ch.setLevel(logging.DEBUG)
-        logger.addHandler(ch)
+        parent_logger.addHandler(ch)
 
 
 class ColoredLogFormatter(logging.Formatter):
     """A logging formatter that add console color codes."""
+    __BLUE = '\033[94m'
     __GREEN = '\033[92m'
     __RED = '\033[91m'
+    __RED_BG = '\033[41m'
     __YELLOW = '\033[33m'
     __ENDC = '\033[0m'
     
+    def _format_message_level(self, message, level):
+        colors = {
+            'INFO': self.__GREEN,
+            'WARNING': self.__YELLOW,
+            'ERROR': self.__RED,
+            'CRITICAL': self.__RED_BG,
+        }
+        if level in colors.keys():
+            message = "{color}{message}{ending}".format(color=colors[level],
+                                                        message=message,
+                                                        ending=self.__ENDC)
+        return message
+    
     def formatMessage(self, record):
-        if record.levelname=='INFO':
-            record.message = self.__GREEN + record.message + self.__ENDC
-        elif record.levelname == 'WARNING':
-            record.message = self.__YELLOW + record.message + self.__ENDC
-        elif record.levelname == 'ERROR':
-            record.message = self.__RED + record.message + self.__ENDC
+        record.message = self._format_message_level(record.message, record.levelname)
         return super().formatMessage(record)
